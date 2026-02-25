738c4c7d2b02fbc8eb4948e5808b61abd884b809
Ariel Poghon <mnangatariel27@gmail.com>
2026-02-25 16:00:05 +0300

    Update hello-wave.tsx

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -5,8 +5,8 @@ export function HelloWave() {
     <Animated.Text
       style={{
         fontSize: 28,
-        lineHeight: 32,
-        marginTop: -6,
+        lineHeight: 32, 
+        marginTop: -6, 
         animationName: {
           '50%': { transform: [{ rotate: '25deg' }] },
         },
