528132f586ea5b33c7a7215a435493e508a33ce9
Ariel Poghon <mnangatariel27@gmail.com>
2026-02-25 16:00:23 +0300

    Update hello-wave.tsx

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -1,8 +1,8 @@
-import Animated from 'react-native-reanimated';
+import Animated from 'react-native-reanimated'; 
 
-export function HelloWave() {
-  return (
-    <Animated.Text
+export function HelloWave() { 
+  return ( 
+    <Animat ed.Text
       style={{
         fontSize: 28,
         lineHeight: 32, 
