6fc53127ad634cc9296ae7011157f9608da1e821
Ariel Poghon <mnangatariel27@gmail.com>
2026-02-28 10:57:47 +0300

    Fix import statements for Video and router

--- app/index.tsx
+++ app/index.tsx
@@ -18,8 +18,8 @@ import {
   TouchableOpacity,
   View, 
 } from 'react-native';
-import { Video } from 'expo-av'; abc
-import { router } from 'expo-router';  abc
+import { Video } from 'expo-av';
+import { router } from 'expo-router'; 
 import { auth, db } from '../firebase';
 import { doc, getDoc, setDoc, serverTimestamp } from 'firebase/firestore';
 const penguinVideo = require('./grok-video-5ed0a94c-11fd-4596-aa1f-5abc44f7be14.mp4'); 
