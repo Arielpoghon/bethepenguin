56575faf82be163e47a1f8c9279c7c4428c1b339
Ariel Poghon <mnangatariel27@gmail.com>
2026-03-02 11:39:26 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: '145543571006',  
   appId: '1:145543571006:web:28a6f973be2203e755536f', 
 };
-
+ 
 const app = initializeApp(firebaseConfig);
 
 //export
