b8c0608334e7f57f6c4a62016d03dc1c72f19348
Ariel Poghon <mnangatariel27@gmail.com>
2026-03-02 11:38:28 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -7,8 +7,8 @@ const firebaseConfig = {
   authDomain: 'postle-d3ea6.firebaseapp.com',
   projectId: 'postle-d3ea6',
   storageBucket: 'postle-d3ea6.appspot.com',
-  messagingSenderId: '145543571006', 
-  appId: '1:145543571006:web:28a6f973be2203e755536f',
+  messagingSenderId: '145543571006',  
+  appId: '1:145543571006:web:28a6f973be2203e755536f', 
 };
 
 const app = initializeApp(firebaseConfig);
