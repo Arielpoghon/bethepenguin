6e717fe9828f7388dfc625749c7958f04105da49
Ariel Poghon <mnangatariel27@gmail.com>
2026-03-02 11:40:06 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,  // your real key here
   authDomain: 'postle-d3ea6.firebaseapp.com',  
   projectId: 'postle-d3ea6', 
-  storageBucket: 'postle-d3ea6.appspot.com',
+  storageBucket: 'postle-d3ea6.appspot.com', 
   messagingSenderId: '145543571006',  
   appId: '1:145543571006:web:28a6f973be2203e755536f', 
 };
