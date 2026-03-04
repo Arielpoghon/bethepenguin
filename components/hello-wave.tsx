e1e59eb319db8821b972e7ffd70a77daa3041616
Ariel Poghon <mnangatariel27@gmail.com>
2026-03-04 13:37:10 +0300

    Update hello-wave.tsx

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -5,8 +5,8 @@ export function HelloWave() {
       style={{ 
         fontSize: 28, 
         lineHeight: 32,  
-        marginTop: -6,  
-        animationName: { 
+        marginTop: -6,   
+        animationName: {  
           '50%': { transform: [{ rotate: '25deg' }] }, 
         },
         animationIterationCount: 4,
