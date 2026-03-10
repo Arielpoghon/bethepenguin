f90c6fd6bae98992b278eb308ce69c3cbddfd544
Ariel Poghon <mnangatariel27@gmail.com>
2026-03-10 17:52:40 +0300

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,8 +19,8 @@ export default function TabLayout() {
     >
       <Tabs.Screen
         name="home"
-        options={{
-          title: 'Home',
+        options={{ 
+          title: 'Home', 
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="house.fill" color={color} />
           ),
