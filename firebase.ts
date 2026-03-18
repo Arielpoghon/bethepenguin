7844dfb478e335eda82e5fd4082804bc376c3dd7
Ariel Poghon <mnangatariel27@gmail.com>
2026-03-18 09:13:31 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,4 +1,4 @@
-removeme import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'; 
 import { getFirestore } from 'firebase/firestore'; 
 
