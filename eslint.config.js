5c51e1d739280145416406cdb0310d3609dd218f
Ariel Poghon <mnangatariel27@gmail.com>
2026-04-29 16:29:54 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 // https://docs.expo.dev/guides/using-eslint /
+/*yoh remove this line */
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat'); 
 
